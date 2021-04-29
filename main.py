5141b301f2f35f07f9171b378d8e007b0ee6a442
Leandro <leandro.padulla@gmail.com>
2021-04-29 17:55:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 #!usr/bin/env python3
 import os
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import utils.tables
+import black_scholes
 from utils.data_handling import *
-from black_scholes_functions import *
 
 pd.options.display.float_format = "{:,.2f}".format
 
-NOTIONAL = - 50_000_000
 EXCEL_OUTPUT_FILE = "greeks.xlsx"
 MAIN_DIRECTORY = os.path.dirname(__file__)
 
-def example() -> dict:
+def parse_extension_of_data(file: str) -> tuple:
+    if "xlsx" in file: 
+        calls, puts = fetch_data_from_excel(file)
+    elif "pickle" in file: 
+        calls, puts = fetch_data_from_pickle(file)
+    else:
+        print("ERROR: Cannot parse database file")
+        sys.exit(1)
 
-    params = dict()
-    params['S'] = 20.1594
-    params['K'] = 20.50
-    params['T'] = 12 / 365
-    params['v'] = 12.868 / 100
-    params['r'] = 3.847 / 100
-    params['q'] = 0
+    return calls, puts
 
-    return params
-
-def main():
+def main(file: str) -> None:
 
     # interval = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25,30,40,50]) / 100
     interval = np.array([1, 2, 5, 7, 10]) / 100
     main_interval = np.concatenate((-np.flip(interval), 0, interval), axis = None)
+<<<<<<< HEAD
 
     #calls, puts = fetch_data_from_pickle("data.pickle")
     calls, puts = fetch_data_from_excel(os.path.join(MAIN_DIRECTORY, "data", "plan_base.xlsx"))
+=======
+    
+    calls, puts = parse_extension_of_data(file)
+>>>>>>> 9ed6ffc5178800d7e7db78cf296bb5dc12fd2706
     call_options, call_notionals, call_names = extract_option_params(calls)
     put_options, put_notionals, put_names = extract_option_params(puts)
 
     call_options.S = generate_spot_interval(call_options.S, main_interval)
     put_options.S = generate_spot_interval(put_options.S, main_interval)
 
-    call_deltas = black_scholes_call_dollar_delta(call_notionals, call_options)
-    put_deltas = black_scholes_put_dollar_delta(put_notionals, put_options)
+    call_deltas = black_scholes.call_dollar_delta(call_notionals, call_options)
+    put_deltas = black_scholes.put_dollar_delta(put_notionals, put_options)
 
-    call_gammas = black_scholes_call_dollar_gamma(call_notionals, call_options)
-    put_gammas = black_scholes_put_dollar_gamma(put_notionals, put_options)
+    call_gammas = black_scholes.call_dollar_gamma(call_notionals, call_options)
+    put_gammas = black_scholes.put_dollar_gamma(put_notionals, put_options)
 
     column_names = ["Delta {:}%".format(int(100 * x)) for x in main_interval]
     table_delta = consolidate_call_put_into_dataframe(call_deltas, call_names, put_deltas, put_names, column_names)
@@ -66,5 +70,5 @@ def main():
     utils.tables.save_table_heatmap(p_gamma, os.path.join(MAIN_DIRECTORY, "data", "table_gamma.png"))
     plt.show()
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": main(sys.argv[1])
 
